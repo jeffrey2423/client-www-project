89d32bba1331427c05a20c4863615dda09c4cbb0
jeffrey2423 <riosjeff24@gmail.com>
2020-12-11 11:39:11 -0500

    Release_11/12/2020

---
+++ client/src/config/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    // SOCKET_SERVER_URI: "//localhost:4000",
+    SOCKET_SERVER_URI: "//backend-www.herokuapp.com",
+    // QUERY_SERVER_URI: "http://localhost:4000/",
+    QUERY_SERVER_URI: "https://backend-www.herokuapp.com/", 
+    ENCRYPTION_SECRET_KEY:  "www-project",
+}
+
+// git init
+// git add .
+// git commit -m "heroku deploy"
+// heroku login
+// heroku create
+// git remote add heroku linkrepo
+// git push heroku master
+// heroku logs
+// heroku open
